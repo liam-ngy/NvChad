c2bb5e23dd200ec7f6cfb86b8882fa45f236793e
Hoang D. Nguyen <hdn.account@icloud.com>
2023-02-25 23:12:08 +1100

    tmp

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -328,6 +328,8 @@ local plugins = {
     cmd = "Copilot",
     config = function()
       vim.g.copilot_no_tab_map = true
+      vim.g.copilot_assume_mapped = true
+      vim.g.copilot_tab_fallback = ""
       vim.api.nvim_set_keymap("i", "<C-Enter>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
     end,
   },
